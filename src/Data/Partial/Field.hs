3693467412065b35c0c1853697658682f507aa0b
Tom Harding <tomjharding@live.co.uk>
2019-04-08 21:33:44 +0100

    Documentation and type errors [Field]
    There's now a custom type error, and more useful examples in the code.

--- src/Data/Partial/Field.hs
+++ src/Data/Partial/Field.hs
@@ -10,27 +10,46 @@
 {-# LANGUAGE UndecidableInstances   #-}
 module Data.Partial.Field where
 
-import Control.Lens (Lens')
+import Control.Lens (Lens', dimap)
 import Data.Partial.Types (Partial (..), Partial_)
 import Data.Kind (Type)
 import GHC.Generics
-import GHC.TypeLits (Symbol)
+import GHC.TypeLits (ErrorMessage (..), Symbol, TypeError)
 
--- | For types with named fields, we can use a 'Control.Lens.Lens' to access
--- the partial fields. Note that we use a 'Control.Lens.Lens' to the 'Maybe'
--- rather than a 'Control.Lens.Prism' because we want the option to "remove"
--- partial data by setting a field to 'Nothing'.
+-- | A la @generic-lens@, we are able to focus on a particular field within our
+-- partial structure. We use a lens to a 'Maybe' rather than a prism to allow
+-- users to /delete/ fields within a partial structure.
+--
+-- >>> :set -XDeriveGeneric -XFlexibleContexts
+-- >>> import Control.Lens
+-- >>> import Data.Partial.Build
+--
+-- We can focus in on particular fields within our structure by type-applying
+-- the name:
+--
+-- >>> data User = User { id :: Int, name :: String } deriving Generic
+-- >>> mempty @(Partial User) ^. field @"id"
+-- Nothing
+--
+-- >>> toPartial (User 1 "Tom") ^. field @"name"
+-- Just "Tom"
+--
+-- >>> toPartial (User 1 "Tom") ^. field @"shoes"
+-- ...
+-- ... User has no field called "shoes"!
+-- ...
 class HasField' (field :: Symbol) (structure :: Type) (focus :: Type)
     | field structure -> focus where
   field :: Lens' (Partial structure) (Maybe focus)
 
-type family Field (field :: Symbol) (rep :: Type -> Type) :: Maybe Type where
-  Field name (S1   (_ ('Just name) _ _ _) (Rec0 (Maybe focus))) = 'Just focus
-  Field _    (S1    _                      _                  ) = 'Nothing
-  Field name (M1 _  _                      xs                 ) = Field name xs
+-------------------------------------------------------------------------------
 
-  Field field (left :*: right) = Field field left <|> Field field right
-  Field _      _               = 'Nothing
+type family Field (field :: Symbol) (rep :: Type -> Type) :: Maybe Type where
+  Field s (S1 (_ ('Just s) _ _ _) (_ (Maybe x))) = 'Just x
+  Field s (M1  _                   _         xs) = Field s xs
+
+  Field s (l :*: r) =  Field s l <|> Field s r
+  Field _  _        = 'Nothing
 
 type family (x :: Maybe k) <|> (y :: Maybe k) :: Maybe k where
   'Just x <|> y = 'Just x
@@ -42,11 +61,14 @@ class GHasField' (field :: Symbol) (rep :: Type -> Type) (focus :: Type)
 
 instance GHasField' field inner focus
     => GHasField' field (M1 index meta inner) focus where
-  gfield = go . gfield @field
-    where go f = fmap M1 . f . unM1
+  gfield = dimap unM1 (fmap M1) . gfield @field
 
 instance GHasField' field (K1 index (Maybe focus)) focus where
-  gfield f = fmap K1 . f . unK1
+  gfield = dimap unK1 (fmap K1)
+
+instance GHasFieldProduct (Field field left) field (left :*: right) focus
+    => GHasField' field (left :*: right) focus where
+  gfield = gfieldProduct @(Field field left) @field
 
 class GHasFieldProduct
     (goal  ::   Maybe Type)
@@ -66,12 +88,25 @@ instance (GHasField' field right focus)
   gfieldProduct = go . gfield @field
     where go f (left :*: right) = fmap (left :*:) (f right)
 
-instance GHasFieldProduct (Field field left) field (left :*: right) focus
-    => GHasField' field (left :*: right) focus where
-  gfield = gfieldProduct @(Field field left) @field
+type HasField_ (field :: Symbol) (structure :: Type)
+  = AssertHasField field structure (Field field (Partial_ structure))
 
-instance (Generic structure, GHasField' field (Partial_ structure) focus)
-    => HasField' field structure focus where
-  field = go . gfield @field
-    where go f (Partial inner) = fmap Partial (f inner)
+type family AssertHasField
+    (field     ::     Symbol)
+    (structure ::       Type)
+    (goal      :: Maybe Type) where
+  AssertHasField _     _        ('Just xs) = xs
+  AssertHasField field structure 'Nothing  = TypeError
+    ( 'ShowType structure
+        ':<>: 'Text " has no field called \""
+        ':<>: 'Text field
+        ':<>: 'Text "\"!"
+    )
+
+instance
+    ( Generic structure
+    , GHasField' field (Partial_ structure) focus
+    , HasField_ field structure ~ focus
+    ) => HasField' field structure focus where
+  field = dimap runPartial (fmap Partial) . gfield @field
 
