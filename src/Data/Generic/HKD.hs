e1b89c1dd108f135909b8b75fb33ecba0ab4b6f8
Tom Harding <tomjharding@live.co.uk>
2019-04-13 00:59:33 +0100

    Finishing touches
    Prepping for hackage :)

--- src/Data/Generic/HKD.hs
+++ src/Data/Generic/HKD.hs
@@ -1,3 +1,11 @@
+{-|
+Module      : Data.Generic.HKD
+Description : A generic-based HKD decorator for ADTs.
+Copyright   : (c) Tom Harding, 2019
+License     : MIT
+Maintainer  : tom.harding@habito.com
+Stability   : experimental
+-}
 module Data.Generic.HKD
   ( module Exports
   ) where
